e9c7466fd14ea88a94d216c277401c61b595a369
riefive <rie.five@gmail.com>
2023-06-21 16:32:40 +0700

    modify apifetch with options

--- src/extends/helpers/apifetch.js
+++ src/extends/helpers/apifetch.js
@@ -4,6 +4,16 @@ AbortSignal.timeout ??= function timeout(ms) {
 	return abortControl.signal
 }
 
-const apiFetch = async () => {}
+const apiFetch = async () => {
+	/*
+  const options = { cache: 'no-cache', method: method.toUpperCase(), headers: { 'Content-Type': 'application/json' } }
+	if (method === 'get' && params) {
+		urlTargetServer += '?' + new URLSearchParams(params).toString()
+	} else {
+		options.body = JSON.stringify(body)
+	}
+	options.signal = AbortSignal.timeout(Number(apiTimeMs) * 1000)
+  */
+}
 
 export { apiFetch }
