e73ada27cdccb2e38d04cfc136b5972cd0191ee6
riefive <rie.five@gmail.com>
2023-06-22 06:33:53 +0700

    add menubar component

--- src/components/layouts/LayoutMain.vue
+++ src/components/layouts/LayoutMain.vue
@@ -1,10 +1,13 @@
 <script setup>
+import Menubar from '@/components/templates/Menubar.vue'
 import Navbar from '@/components/templates/Navbar.vue'
 </script>
 
 <template>
 	<div class="relative flex flex-row min-h-screen">
-		<aside class="bg-neutral w-[10%] small:hidden"></aside>
+		<aside class="bg-gray-600 w-[15%] text-white small:hidden">
+			<menubar />
+		</aside>
 		<div class="flex flex-col flex-grow">
 			<header><navbar /></header>
 			<main class="flex-auto flex-grow"><slot /></main>
---
+++ src/components/templates/Menubar.vue
@@ -0,0 +1,7 @@
+<template>
+	<ul class="menu bg-gray-600 mt-[50px] p-2">
+		<li><a>Item 1</a></li>
+		<li><a>Item 2</a></li>
+		<li><a>Item 3</a></li>
+	</ul>
+</template>
