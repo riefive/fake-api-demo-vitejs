2df6bc9a51416966e228bb7afe6dff3c4cc60424
riefive <rie.five@gmail.com>
2023-06-21 17:22:03 +0700

    update function apifetch

--- src/extends/helpers/apifetch.js
+++ src/extends/helpers/apifetch.js
@@ -4,16 +4,33 @@ AbortSignal.timeout ??= function timeout(ms) {
 	return abortControl.signal
 }
 
-const apiFetch = async () => {
-	/*
-  const options = { cache: 'no-cache', method: method.toUpperCase(), headers: { 'Content-Type': 'application/json' } }
-	if (method === 'get' && params) {
-		urlTargetServer += '?' + new URLSearchParams(params).toString()
-	} else {
-		options.body = JSON.stringify(body)
+const apiFetch = async (url, method, bodies = null, settings = null) => {
+	const headers = new Headers()
+	const options = { method: method.toUpperCase() }
+	const type = settings?.type || 'text/plain'
+	const timeout = settings?.timeout || 10
+	const token = settings?.token || null
+	const tokenType = settings?.tokenType || 'Bearer'
+	const cacheType = settings?.cache || 'default' // default | no-cache
+	if (token) {
+		headers.append('Authorization', `${tokenType} ${token}`)
 	}
-	options.signal = AbortSignal.timeout(Number(apiTimeMs) * 1000)
-  */
+	if (method === 'get') {
+		url += bodies && Object.keys(bodies).length > 0 ? '?' + new URLSearchParams(params).toString() : ''
+	} else if (['post', 'put', 'patch'].includes(method)) {
+		if (/json/i.test(type)) {
+			headers.append('Content-Type', 'application/json')
+			options.body = JSON.stringify(bodies)
+		}
+		if (/object/i.test(type)) {
+			const formData = new FormData()
+			for (let key in bodies) formData.append(key, bodies[key])
+			options.body = formData
+		}
+	}
+	options.cache = cacheType
+	options.headers = headers
+	options.signal = AbortSignal.timeout(Number(timeout) * 1000)
 }
 
 export { apiFetch }
