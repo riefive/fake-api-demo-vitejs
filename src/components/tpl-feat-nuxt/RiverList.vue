4d57d8758bd2ae2760fe880f68707ff16c17bfb4
riefive <rie.five@gmail.com>
2023-06-22 12:26:58 +0700

    add nuxt river page

--- src/components/tpl-feat-nuxt/RiverList.vue
+++ src/components/tpl-feat-nuxt/RiverList.vue
@@ -1,8 +1,8 @@
 <script setup>
 import { onMounted, ref } from 'vue'
-import { columns } from '@/extends/models/nuxt_mountain_model'
-import { transformToClient } from '@/extends/scripts/nuxt_mountain_script'
-import { requestNuxtMountain } from '@/extends/services/nuxt_service'
+import { columns } from '@/extends/models/nuxt_river_model'
+import { transformToClient } from '@/extends/scripts/nuxt_river_script'
+import { requestNuxtRiver } from '@/extends/services/nuxt_service'
 import { sleep } from '@/extends/helpers/utils'
 import TableShow from '@/components/commons/TableShow.vue'
 
@@ -11,7 +11,7 @@ const items = ref([])
 
 onMounted(async () => {
 	loading.value = true
-	const response = await requestNuxtMountain()
+	const response = await requestNuxtRiver()
 	if (response?.data) {
 		items.value = response?.data?.map((item, i) => {
 			const itemNewer = transformToClient(item)
