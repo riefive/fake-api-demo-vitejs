a3758a63e4957f64d7a0daf86161a70eb618ce13
riefive <rie.five@gmail.com>
2023-06-22 07:00:22 +0700

    add clicked menu

--- src/components/templates/Menubar.vue
+++ src/components/templates/Menubar.vue
@@ -1,11 +1,18 @@
 <script setup>
+import { useRouter } from 'vue-router'
 import menus from '@/extends/settings/menus'
+
+const router = useRouter()
+
+const onClicked = (path) => {
+	router.push(path === 'dashboard' ? '/' : `/${path}`)
+}
 </script>
 
 <template>
 	<ul class="menu menu-md bg-gray-600 mt-[50px] p-2">
 		<li v-for="(menu, i) in menus" :key="i">
-			<span v-if="menu.type === 'text'" class="mdil" :class="menu.icon">
+			<span v-if="menu.type === 'text'" class="mdil" :class="menu.icon" @click="onClicked(menu.id)">
 				<a>{{ menu.text }}</a>
 			</span>
 			<details v-else-if="menu.type === 'dropdown'" :open="false">
@@ -14,7 +21,7 @@ import menus from '@/extends/settings/menus'
 					<a>{{ menu.text }}</a>
 				</summary>
 				<ul>
-					<li v-for="(subMenu, j) in menu.menus" :key="j">
+					<li v-for="(subMenu, j) in menu.menus" :key="j" @click="onClicked(menu.id + '-' + subMenu.id)">
 						<span class="mdil" :class="subMenu.icon">
 							<a>{{ subMenu.text }}</a>
 						</span>
--- src/extends/settings/menus.js
+++ src/extends/settings/menus.js
@@ -1,6 +1,9 @@
 const subMenuOfNuxt = [
 	{ id: 'beer', text: 'List of Beers', type: 'text', icon: 'mdil-grid' },
-	{ id: 'mountain', text: 'List of Mountains', type: 'text', icon: 'mdil-grid' }
+	{ id: 'mountain', text: 'List of Mountains', type: 'text', icon: 'mdil-grid' },
+	{ id: 'planet', text: 'List of Planets', type: 'text', icon: 'mdil-grid' },
+	{ id: 'post', text: 'List of Posts', type: 'text', icon: 'mdil-grid' },
+	{ id: 'river', text: 'List of Rivers', type: 'text', icon: 'mdil-grid' }
 ]
 
 const menus = [
