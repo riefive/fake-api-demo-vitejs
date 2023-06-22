733965144fe6f93eee81ff1ced2be7a78a72b372
riefive <rie.five@gmail.com>
2023-06-22 12:25:09 +0700

    add nuxt post page

---
+++ src/extends/scripts/nuxt_post_script.js
@@ -0,0 +1,17 @@
+const transformToClient = (item) => {
+	return {
+		index: 0,
+		title: item.title,
+		slug: item.slug,
+		description: item.description?.length > 35 ? String(item.description || '').substring(0, 32) + '...' : item.description,
+		height: item.height,
+		continent: item.continent,
+		countries: (item.countries || [])?.join(','),
+		image: item.image,
+		dir: item.dir,
+		path: item.path,
+		updatedAt: String(item.updatedAt || '').substring(0, 10)
+	}
+}
+
+export { transformToClient }
